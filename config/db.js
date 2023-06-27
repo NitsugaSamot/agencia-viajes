80bc9c8e558aa77a642ca347bd1bff04734f14a5
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-06-27 13:31:32 -0300

    Actualizaciones en las variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv/config'
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, '', {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: '127.0.0.1',
     port: '3306',
     dialect: 'mysql',
